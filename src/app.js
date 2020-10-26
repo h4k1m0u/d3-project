b302481472a1dd32cd622f26f67a0979119ed3ae
h4k1m0u <h.benoudjit@gmail.com>
2020-10-26 20:15:01 +0100

    Show tooltip on bar hover and change its opacity

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const scaleY = d3.scaleLinear()
 // container (leave space for axis ticks)
 const svg = d3.select('body')
   .insert('svg', ':first-child')
-  .attr('width', widthBar * data.length + axisSize)
+  .attr('width', widthBar * data.length + axisSize + 20)
   .attr('height', height);
 
 // each bar is an svg group (contains rect + text)
@@ -33,13 +33,63 @@ const bars = svg.selectAll(null)
 bars.append('rect')
   .attr('width', `${widthBar}`)
   .attr('height', (d) => `${height - (2 * axisSize) - scaleY(d.value)}px`)
+  .attr('opacity', 1.0)
   .attr('fill', 'steelblue');
 
 bars.append('text')
   .attr('fill', 'white')
+  .attr('font-family', 'sans-serif')
+  .attr('font-size', 10)
   .attr('dominant-baseline', 'hanging')
   .attr('writing-mode', 'tb')
+  .attr('pointer-events', 'none')
   .text((d) => d.name);
+
+// tooltip shown on hover
+const tooltip = svg.append('g')
+  .style('visibility', 'hidden');
+tooltip.append('rect')
+  .attr('y', -20)
+  .attr('opacity', 0.5)
+  .attr('width', 20)
+  .attr('height', 19)
+  .attr('fill', 'green');
+tooltip.append('text')
+  .attr('fill', 'white')
+  .attr('dominant-baseline', 'auto')
+  .attr('text-anchor', 'middle')
+  .attr('font-family', 'sans-serif')
+  .attr('font-size', 10)
+  .attr('x', widthBar / 2)
+  .attr('y', -5);
+
+// show data tooltip on hover & change rect opacity
+bars.on('mouseover', function onMouseOver() {
+  const rect = d3.select(this)
+    .select('rect');
+
+  const { x, y } = rect.node().getBoundingClientRect();
+  const { value } = rect.data()[0];
+  tooltip.style('visibility', 'visible')
+    .attr('transform', () => `translate(${x}, ${y})`);
+  tooltip.select('text')
+    .text(value);
+
+  rect.transition()
+    .duration(500)
+    .ease(d3.easeLinear)
+    .attr('opacity', 0.7);
+});
+bars.on('mouseout', function onMouseOut() {
+  tooltip.style('visibility', 'hidden');
+
+  d3.select(this)
+    .select('rect')
+    .transition()
+    .duration(500)
+    .ease(d3.easeLinear)
+    .attr('opacity', 1.0);
+});
 
 // draw axes
 const gBottom = svg.append('g')
@@ -55,6 +105,7 @@ svg.append('text')
   .attr('font-size', 10)
   .attr('dominant-baseline', 'middle')
   .attr('text-anchor', 'middle')
+  .attr('pointer-events', 'none')
   .attr('x', axisSize / 2)
   .attr('y', axisSize / 2)
   .text('Age');
